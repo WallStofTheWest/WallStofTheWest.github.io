2eb36e7964758b988775326924abf866b7eb8a24
Viv Krish <43484434+catchvivkrish@users.noreply.github.com>
2019-02-23 14:04:19 -0800

    Add files via upload

---
+++ plots.js
@@ -0,0 +1,33 @@
+// define the trace function.
+function makeTrace() {
+  return {
+    x: data.columns,
+    y: data.data[i],
+    mode: "lines+markers",
+	type: "scatter",
+	name: data.data[i][0],
+	marker: {
+//    color: "#2077b4",
+    symbol: "hexagram"
+  }
+  }
+}
+
+// Create empty data array for the plot
+var plot_data = []
+// Create the Traces for each plot
+for (i=0; i<data.data.length; i++) {
+var trace = makeTrace(i)
+plot_data.push(trace)
+}
+
+// Define the plot layout
+var layout = {
+  title: "Year on Year Sector returns over a 20 year period",
+  xaxis: { title: "Year" },
+  yaxis: { title: "Year on Year Return (%)" }
+};
+
+// Plot the chart to a div tag with id "plot"
+Plotly.newPlot("plot", plot_data, layout);
+
